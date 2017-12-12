b19943a66336f61932c4d7edd6f656ed5204007a
plesiosaure <42@e-xode.com>
2017-12-12 14:32:15 +0100

    first commit from crepe-bot

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const axios = require('axios')
 
 const app = express()
 const PORT = 5000
@@ -9,3 +10,24 @@ app.use(bodyParser.json())
 app.listen(PORT, () => {
   console.log('Server is running')
 })
+app.all('/crepes', (req, res) => {
+  const ingredient = req.body.conversation.memory.garniture.value
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=PWUkVcwDtpUD9eLhxw19tt0bfjV6hMvm&q=${ingredient}`
+
+  axios.get(url).then(giphyRes => {
+    const body = giphyRes.data
+    const gifs = body.data
+
+    const rand = Math.floor(Math.random() * gifs.length);
+    const gif = gifs[rand]
+    const gifUrl = gif.images.original_still.url
+
+    const response = {
+      replies: [{
+          type: 'picture',
+            content: gifUrl
+        }]
+    }
+    res.send(response)
+  })
+ })
